3bd910a4660bd7f7aa8ea3d48e233c86cc88ebfb
wxnnvs <136503688+wxnnvs@users.noreply.github.com>
2023-09-14 23:45:36 +0200

    attempt to get spider plugin working

--- plugins/spider.js
+++ plugins/spider.js
@@ -2,6 +2,6 @@ PluginAPI.require("player");
 PluginAPI.addEventListener("update", ()=>{ //Every client tick
   if(PluginAPI.player.isCollidedHorizontally){ //Is player walking in to a wall?
     PluginAPI.player.motionY = 0.2; //Default ladder speed
-    PluginAPI.updateComponent("player"); //Tell api that player needs updating
+    //Tell api that player needs updating
   }
 });
